369a49c1f01155d44ddbd5d574f537c7ffa6b5d3
PetersonSantos9K <petersonsantos9k@gmail.com>
2026-04-29 21:04:58 -0300

    feat: created User and Resource

---
+++ src/main/java/com/petersonexercicio/course/resources/UserResource.java
@@ -0,0 +1,18 @@
+package com.petersonexercicio.course.resources;
+
+import com.petersonexercicio.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L, "Maria", "maria@gmai.com", "99999", "12345");
+        return ResponseEntity.ok().body(u);
+    }
+}
