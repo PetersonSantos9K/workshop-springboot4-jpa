74b84540c0429b9920322eb7e65ee9baa26bb0da
PetersonSantos9K <petersonsantos9k@gmail.com>
2026-05-02 02:27:00 -0300

    feat: registrando os components e realizando tests

--- src/main/java/com/petersonexercicio/course/repositories/UserRepository.java
+++ src/main/java/com/petersonexercicio/course/repositories/UserRepository.java
@@ -2,5 +2,7 @@ package com.petersonexercicio.course.repositories;
 
 import com.petersonexercicio.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> { }
